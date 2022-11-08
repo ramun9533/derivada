9351c6d87cb72b4774ef7e7c744d0b0714785f42
William Elí O.L <ramun346416@gmail.com>
2022-11-07 23:33:28 -0600

    Carga de programa

---
+++ derivada.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main()
+
+{
+int a, b, c, d, n, m, p; //la variable d es la constante
+printf("\n Favor de intruducir las constantes de la ecuacion (a, b, c, d) y sus exponentes (n, m, p) \n");
+printf("\n aX^n+bX^m+cX^p+d \n");
+printf("\n Favor de proporcionar la constante ""a"" \n"); // carga de exponentes y constantes
+scanf("%d",&a);
+printf("\n Favor de proporcionar el exponente ""n"" \n");
+scanf("%d",&n);
+printf("\n Favor de proporcionar la constante ""b"" \n");
+scanf("%d",&b);
+printf("\n Favor de proporcionar el exponente ""m""\n");
+scanf("%d",&m);
+printf("\n Favor de proporcionar la constante ""c"" \n"); // carga de exponentes y constantes
+scanf("%d",&c);
+printf("\n Favor de proporcionar el exponente ""p"" \n");
+scanf("%d",&p);
+printf("\n Favor de proporcionar la constante ""d"" \n"); // carga de exponentes y constantes
+scanf("%d",&d);
+}
