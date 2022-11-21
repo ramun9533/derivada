504702de5b2a52d53ddc509dae94b32a102d1a97
William Elí O.L <ramun346416@gmail.com>
2022-11-21 17:04:58 -0600

    Se agrega las primeras variables
    Falta integrar

--- derivada.c
+++ derivada.c
@@ -10,14 +10,23 @@ printf("\n Favor de proporcionar la constante ""a"" \n"); // carga de exponentes
 scanf("%d",&a);
 printf("\n Favor de proporcionar el exponente ""n"" \n");
 scanf("%d",&n);
-printf("\n Favor de proporcionar la constante ""b"" \n");
-scanf("%d",&b);
-printf("\n Favor de proporcionar el exponente ""m""\n");
-scanf("%d",&m);
-printf("\n Favor de proporcionar la constante ""c"" \n"); // carga de exponentes y constantes
-scanf("%d",&c);
-printf("\n Favor de proporcionar el exponente ""p"" \n");
-scanf("%d",&p);
-printf("\n Favor de proporcionar la constante ""d"" \n"); // carga de exponentes y constantes
-scanf("%d",&d);
+// printf("\n Favor de proporcionar la constante ""b"" \n");
+// scanf("%d",&b);
+// printf("\n Favor de proporcionar el exponente ""m""\n");
+// scanf("%d",&m);
+// printf("\n Favor de proporcionar la constante ""c"" \n"); // carga de exponentes y constantes
+// scanf("%d",&c);
+// printf("\n Favor de proporcionar el exponente ""p"" \n");
+// scanf("%d",&p);
+// printf("\n Favor de proporcionar la constante ""d"" \n"); // carga de exponentes y constantes
+// scanf("%d",&d);
+//aX^n+bX^m+cX^p+d
+// printf("\n %dX^%d+%dX^%d+%dX^%d+%dX \n", a, n, b, m, c, p, d);
+//Aqui empieza la integral
+    // n++;
+    // if (a>1 && n>1 )
+    // printf("%dX^%d",a,n);
+    // if ((a>1 && n>1) && (a==n) )
+    // printf("X^%d",a,n);
+printf("(""%dX^%d"")/%d",a,n,n); // Prueba impresion de parentesis
 }
